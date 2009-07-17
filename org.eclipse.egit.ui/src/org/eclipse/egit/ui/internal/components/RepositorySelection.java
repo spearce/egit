0aced3cbc6551d5911844105bae19fbcb925b1f9
Shawn O. Pearce <spearce@spearce.org>
2009-07-17 12:22:16 -0700

    Correct reference to EPL in source headers
    The Eclipse Foundation prefers that the reference to the license
    be made to the online version rather than to a copy provided only
    in the distribution.
    
    Signed-off-by: Shawn O. Pearce <spearce@spearce.org>

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/components/RepositorySelection.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/components/RepositorySelection.java
@@ -3,7 +3,8 @@
  *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
- * See LICENSE for the full license text, also available.
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
  *******************************************************************************/
 
 package org.eclipse.egit.ui.internal.components;
